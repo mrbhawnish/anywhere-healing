d0841c29610e5d18c2c799afab4b0e051f759553
Bhawnish Kumar <bhawnish.kumar@24g.com>
2022-09-09 01:27:28 -0700

    allcommit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`SERVER IS UP ON PORT ${port}`)
